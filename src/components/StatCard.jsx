5327b45e7511e8ff1c427f046b21b5c9d7ea4da5
sellxilips <sellxilips@gmail.com>
2025-09-30 16:29:15 +0700

    commit

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -8,7 +8,7 @@ const StatCard = ({ title, value }) => {
 }).format(value);
 
     return (
-        <div className="w-full md-1/2 bg-slate-800/20 backdrop-blur-md border border-white/10 rounded-2xl p-6 text-center shadow-lg hover-white/20 hover-slate-800/30 transition-all duration-300">
+        <div className="w-full md-1/2 bg-slate-800/20 backdrop-blur-md border border-white/10 rounded-2xl p-3 text-center shadow-lg hover-white/20 hover-slate-800/30 transition-all duration-300">
             <p className="text-xs sm-sm text-gray-400 tracking-wider mb-2">{title}</p>
             <p className="text-2xl sm-3xl font-semibold text-white">{formattedValue}</p>
         </div>
